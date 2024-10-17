0224132ec724dcef0bc3a55895c779604e7a5939
jasopayaso <jacintocastro464@outlook.com>
2024-10-16 21:22:20 -0500

    big update

--- main.py
+++ main.py
@@ -24,23 +24,47 @@ def main():
     #html and beautiful soup for results
     result_html = get_results_data(number, 15)
     result_soup = BeautifulSoup(result_html, 'html.parser')
-    result_body = result_soup.find(class_ = 'match-fixture')
-    result_body_text = result_body.text
+    #every game starts with the class match-fixture
+    result_body = result_soup.find_all(class_ = 'match-fixture', limit= None)
+    # log_info('RESULT BODY: {}'.format(result_body))
+    #gets the length of the result_body which should be the same as the amount of games in that season
+    count = len(result_body)
 
-    result = [word for word in result_body_text.split() if word]
-    home_team = result[0]
-    home_score = result[2][0]
-    away_team = result[3]
-    away_score = result[2][2]
+    #iterates over the match-fixture that holds the info about the game
+    for teams in result_body:
+        #finds all the teams names in result_body
+        
+        team_names = teams.find_all('span', class_ = 'match-fixture__short-name')
+        team_names_text = [team.get_text() for team in team_names]
+        
+       #gets the respective teams names from the text array
+        home_team = team_names_text[0]
+        away_team = team_names_text[1]
 
-    log_info('RESULT BODY: {}'.format(result_body_text))
-    log_info('RESULT: {}'.format(result))
-    log_info('HOME TEAM: {}'.format(home_team))
-    log_info('AWAY TEAM: {}'.format(away_team))
-    log_info('HOME SCORE: {}'.format(home_score))
-    log_info('AWAY SCORE: {}'.format(away_score))
- 
+        #finds the scores for the respective team
+        team_scores = teams.find_all('span', class_ = 'match-fixture__score')
+        team_scores_text = [score.get_text() for score in team_scores]
+        home_score = team_scores_text[0][0]
+        away_score = team_scores_text[0][2]
+        
+        #compares the scores to see who won
+        if int(home_score) > int(away_score):
+            winner = home_team
+        elif int(home_score) < int(away_score):
+            winner = away_team
+        else: 
+            winner = 'DRAW'
+        log_info('{} {}-{} {}. Winner: {}'.format(home_team, home_score, away_score, away_team, winner))
+
+        game_id = teams['data-comp-match-item']
+
+        log_info('GAME ID: {}'.format(game_id))
     
+    
+    log_info('TOTAL AMOUNT OF GAMES: {}'.format(count))
+ 
+ 
+    #test comment
 
 if __name__ == "__main__":
     main()
